5a4cb131d0d86a76f36d6dc7bf506753a2aa27bf
Commmodore64 <cesarml2002@hotmail.com>
2023-05-16 11:08:48 -0700

    treejs funcionando

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,100 +1,72 @@
 import { ReactSVG } from "react-svg";
 import barcode from "../img/barcode.svg";
-import * as THREE from "three";
+import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useRef } from "react";
+import { useState } from "react";
 
-// Step 1: Create a scene
-const scene = new THREE.Scene();
+extend({ OrbitControls });
 
-// Step 2: Create a camera
-const camera = new THREE.PerspectiveCamera(
-  20,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  500
-);
-camera.position.set(0, 0, 10);
-
-// Step 3: Create a renderer and add to the DOM
-const renderer = new THREE.WebGLRenderer({ alpha: true }); // set alpha to true for transparent background
-const container = document.createElement("div");
-document.body.appendChild(container);
-container.appendChild(renderer.domElement);
-
-// Step 4: Add a mesh to the scene
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
-cube.position.set(0, 0, 0); // set position
-cube.rotation.set(0.5, 0.5, 0); // set rotation
-cube.scale.set(2, 2, 2); // set scale
-scene.add(cube);
-
-// Agregar el siguiente código después de crear el cubo
-
-let isDragging = false;
-let previousMousePosition = {
-  x: 0,
-  y: 0,
-};
-
-function onDocumentMouseDown(event) {
-  isDragging = true;
-}
-
-function onDocumentMouseMove(event) {
-  if (!isDragging) {
-    return;
-  }
-
-  const deltaMove = {
-    x: event.offsetX - previousMousePosition.x,
-    y: event.offsetY - previousMousePosition.y,
-  };
-
-  const deltaRotationQuaternion = new THREE.Quaternion().setFromEuler(
-    new THREE.Euler(
-      toRadians(deltaMove.y * 1),
-      toRadians(deltaMove.x * 1),
-      0,
-      "XYZ"
-    )
+function Cube() {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01));
+  return (
+    <mesh ref={ref}>
+      <boxGeometry args={[3, 3, 3]} /> {/* Ajusta los valores de escala aquí */}
+      <meshBasicMaterial wireframe={true} />
+    </mesh>
   );
+}
 
-  cube.quaternion.multiplyQuaternions(deltaRotationQuaternion, cube.quaternion);
-  previousMousePosition = {
-    x: event.offsetX,
-    y: event.offsetY,
+function Controls() {
+  const { camera, gl } = useThree();
+  return <orbitControls args={[camera, gl.domElement]} />;
+}
+function InvisibleButton() {
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    setClickCount((prevCount) => prevCount + 1);
+
+    if (clickCount + 1 === 5) {
+      // Aquí puedes realizar el evento que deseas después de 5 clics
+      console.log("Se han realizado 5 clics");
+      window.location.href = "https://i.ibb.co/RHYRPtM/cesarmiron.jpg";
+    }
   };
+
+  const buttonStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "60%",
+    height: "60%",
+    opacity: 0,
+    //pointerEvents: "none",
+  };
+
+  return (
+    <button style={buttonStyle} onClick={handleClick}>
+      boton
+    </button>
+  );
 }
 
-function onDocumentMouseUp() {
-  isDragging = false;
-}
-
-function toRadians(angle) {
-  return angle * (Math.PI / 180);
-}
-
-document.addEventListener("mousedown", onDocumentMouseDown);
-document.addEventListener("mousemove", onDocumentMouseMove);
-document.addEventListener("mouseup", onDocumentMouseUp);
-
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-animate();
-
-function home() {
+function Home() {
   return (
     <div className="relative flex flex-col">
+      <div className="absolute right-48 top-1/2 transform translate-y-44 h-96">
+        <Canvas>
+          <Cube />
+          <Controls />
+        </Canvas>
+      </div>
       <div className="absolute left-0 top-52 text-xs sm:text-sm transform -rotate-90 origin-top-left text-white z-0 w-20 font-mono">
         <h1 className="whitespace-nowrap text-xs mt-2">software developer</h1>
       </div>
       <div className="absolute left-0 top-64 ml-2">
         <ReactSVG src={barcode} />
+        <InvisibleButton />
       </div>
       <div className="absolute left-32 top-52">
         <h1 className="text-[#DBDBDB] font-mono text-3xl">
@@ -108,4 +80,4 @@ function home() {
   );
 }
 
-export default home;
+export default Home;
